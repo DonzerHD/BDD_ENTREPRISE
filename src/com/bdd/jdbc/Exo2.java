a7d69f476ecd5b5cd6de77ecbbb6507428178c9a
Thomas59 <thomas.lemay59@outlook.fr>
2022-12-09 16:37:38 +0100

    exo2

--- src/com/bdd/jdbc/Exo2.java
+++ src/com/bdd/jdbc/Exo2.java
@@ -3,8 +3,7 @@ package com.bdd.jdbc;
 import java.io.FileInputStream;
 import java.util.Properties;
 
-
-public class Exo1 {
+public class Exo2 {
 	public static void main(String[] args) throws Exception{
 	      Properties props = new Properties();
 	      try (FileInputStream fis = new FileInputStream("conf.properties")){
@@ -18,9 +17,10 @@ public class Exo1 {
 			 String password = props.getProperty("jdbc.password");
 			 
 		     ExoJDBC Exo = new ExoJDBC();
-		     Exo.exo1(url, login, password);
+		     Exo.exo2(url, login, password);
 		     
 		     
 			 
 		}
+
 }
--- src/com/bdd/jdbc/ExoJDBC.java
+++ src/com/bdd/jdbc/ExoJDBC.java
@@ -2,22 +2,35 @@ package com.bdd.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLTimeoutException;
+import java.sql.Statement;
 
 public class ExoJDBC {
 
 	/**
-	 * @param url
+	 * méthode qui connecte la base de données à Java
+	 * @param url 
 	 * @param login
 	 * @param password
 	 * @throws SQLException
 	 */
-	public void exo1(String url, String login, String password) throws SQLException {
+	public void exo1(String url, String login, String password) throws SQLTimeoutException{
 		try (Connection connection = DriverManager.getConnection(url, login, password)) {
             System.out.println("Connexion établie");
-		}catch (Exception e) {
+		}catch (SQLException  e) {
 			System.out.println("Connexion non établie");
 		}
 
+	}
+	public void exo2(String url, String login, String password) {
+		try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            String select = "SELECT * FROM emp";
+     
+		}catch (SQLException  e) {
+			System.out.println("Connexion non établie");
+		}
+		
 	}
 }
