024638fea878333ec17b1c4c3b2cb41cbd30e677
DonzerHD <thomas.lemay59@outlook.fr>
2022-12-10 11:18:42 +0100

    ADD employe

--- src/Exercice9/Entreprise.java
+++ src/Exercice9/Entreprise.java
@@ -6,34 +6,42 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLTimeoutException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
- * Class qui va nous permettre d'aller chercher les informations dans la base de données .
+ * Class qui va nous permettre d'aller chercher les informations dans la base de
+ * données .
+ * 
  * @author thomas59
  *
  */
 public class Entreprise {
-        
+
 	/**
-	 * Méthode qui affiche la liste des employés 
+	 * Méthode qui affiche la liste des employés
+	 * 
 	 * @param url
 	 * @param login
 	 * @param password
 	 * @throws SQLTimeoutException
 	 */
-	public void afficheListeEmploye(String url, String login, String password) throws SQLTimeoutException {
+	public void afficheListeEmploye(final String url, final String login, final String password)
+			throws SQLTimeoutException {
 		try (Connection connection = DriverManager.getConnection(url, login, password)) {
 			String select = "SELECT nom,prenom,emploi FROM entreprise.emp";
 			try (Statement statement = connection.createStatement()) {
 				ResultSet resulSet = statement.executeQuery(select);
+				System.out.println("----------------------------------------------");
+				System.out.println("Affichage de la liste des employés : ");
+				System.out.println("----------------------------------------------");
 				while (resulSet.next()) {
 					String nom = resulSet.getString(1);
 					String prenom = resulSet.getString(2);
 					String emploi = resulSet.getString(3);
-       				StringBuilder Affichage = new StringBuilder();
-					Affichage.append("nom : " + nom + " ");
-					Affichage.append("prénom : " + prenom + " ");
-					Affichage.append("emploi : " + emploi + " ");
+					StringBuilder Affichage = new StringBuilder();
+					Affichage.append("Nom : " + nom + " | ");
+					Affichage.append("Prénom : " + prenom + " | ");
+					Affichage.append("Emploi : " + emploi + " | ");
 					System.out.println(Affichage);
 					connection.close();
 
@@ -42,6 +50,70 @@ public class Entreprise {
 
 		} catch (SQLException e) {
 			e.printStackTrace();
+			System.out.println("Connexion non établie");
+		}
+
+	}
+
+	/**
+	 * méthode qui affiche le détail d’un employé .
+	 * 
+	 * @param url
+	 * @param login
+	 * @param password
+	 * @throws SQLTimeoutException
+	 */
+	public void afficheDetailsEmploye(final String url, final String login, final String password)
+			throws SQLTimeoutException {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		boolean donneesExiste = false;
+		try (Connection connection = DriverManager.getConnection(url, login, password)) {
+			System.out.print("Saisissez le nom de l'employé : ");
+			String nomSaisie = sc.nextLine();
+			System.out.print("Saisissez le prénom de l'employé : ");
+			String prenomSaisie = sc.nextLine();
+			String select = "SELECT * FROM entreprise.emp WHERE nom = UPPER('" + nomSaisie + "') AND prenom = UPPER('"
+					+ prenomSaisie + "')";
+			try (Statement statement = connection.createStatement()) {
+				ResultSet resulSet = statement.executeQuery(select);
+				while (resulSet.next()) {
+					int noemp = resulSet.getInt(1);
+					String nom = resulSet.getString(2);
+					String prenom = resulSet.getString(3);
+					String emploi = resulSet.getString(4);
+					int sup = resulSet.getInt(5);
+					String embauche = resulSet.getString(6);
+					double sal = resulSet.getDouble(7);
+					int comm = resulSet.getInt(8);
+					int numS = resulSet.getInt(9);
+					
+					if (nomSaisie.equalsIgnoreCase(nom) && prenomSaisie.equalsIgnoreCase(prenom)) {
+						donneesExiste = true;
+						StringBuilder Affichage = new StringBuilder();
+						System.out.println("----------------------------------------------");
+						System.out.println("Voici le détail de l'employé : ");
+						System.out.println("----------------------------------------------");
+						Affichage.append("no emp : " + noemp + " ");
+						Affichage.append("nom : " + nom + " ");
+						Affichage.append("prénom : " + prenom + " ");
+						Affichage.append("emploi : " + emploi + " ");
+						Affichage.append("sup : " + sup + " ");
+						Affichage.append("date embauche : " + embauche + " ");
+						Affichage.append("salaire : " + sal + " euros ");
+						Affichage.append("commission : " + comm + " ");
+						Affichage.append("numéro service : " + numS);
+						System.out.println(Affichage);
+					}            
+				}
+				if ( donneesExiste == false) {
+					       System.out.println("----------ERREUR------------------------------");
+	                       System.out.println(" Ce prénom ou nom ne correspond à aucun employé.");
+	                       System.out.println("----------ERREUR------------------------------");
+						}
+			}
+
+		} catch (SQLException e) {
 			System.out.println("Connexion non établie");
 		}
 
--- src/Exercice9/Main.java
+++ src/Exercice9/Main.java
@@ -8,10 +8,10 @@ import java.util.InputMismatchException;
 import java.util.Properties;
 import java.util.Scanner;
 
-import Exercice1a8.ExoJDBC_MAIN;
-
 /**
  * Class qui permet de lancer le programme avec le menu .
+ * Pour le fichier PROPERTIES : Je mets les infos pour se connecter à la base de données  dans le fichier "properties" pour 
+ * faire en sorte de changer le mot de passe ou nom d'utilisateur tous les mois pour plus de sécurité .
  * 
  * @author thomas59
  *
@@ -21,6 +21,7 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, SQLTimeoutException {
 
 		// FICHIER PROPERTIES
+		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		Properties props = new Properties();
 		try (FileInputStream fis = new FileInputStream("conf.properties")) {
@@ -28,9 +29,9 @@ public class Main {
 		}
 
 		Class.forName(props.getProperty("jdbc.driver.class"));
-		String url = props.getProperty("jdbc.url");
-		String login = props.getProperty("jdbc.login");
-		String password = props.getProperty("jdbc.password");
+		final String url = props.getProperty("jdbc.url");
+		final String login = props.getProperty("jdbc.login");
+		final String password = props.getProperty("jdbc.password");
 		
 		//Création Entreprise
 		Entreprise entreprise = new Entreprise();
@@ -42,16 +43,20 @@ public class Main {
 			switch (choix) {
 
 			case 1:
-				entreprise.afficheListeEmploye(url, login, password);
-				
+				entreprise.afficheListeEmploye(url, login, password);	
+				retourMenu();
 				break;
 			case 2:
+				entreprise.afficheDetailsEmploye(url, login, password);
+				retourMenu();
 				break;
 			case 3:
 				break;
 			case 4:
 				break;
 			case 5:
+				break;
+			case 6:
 				break;
 			default:
 				System.out.println("Ne correspond à aucune option");
@@ -77,8 +82,29 @@ public class Main {
 		System.out.println("3 - Ajouter un employé ");
 		System.out.println("4 - Modifier un employé à partir de son nom et prénom");
 		System.out.println("5 - Supprimer un employé à partir de son nom et prénom");
+		System.out.println("6 - Fermer le programme");
 		System.out.println("-------------------------");
 		System.out.print("Veuillez sélectionner une option : ");
+	}
+	
+	/**
+	 * Méthode qui va retourner au menu avec un temps d'attente
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 * @throws FileNotFoundException 
+	 * @throws SQLTimeoutException 
+	 */
+	public static void retourMenu() throws SQLTimeoutException, FileNotFoundException, ClassNotFoundException, IOException {
+		try {
+			System.out.println("----------------------------------------------");
+			System.out.println("Retour dans le menu dans quelques secondes ...");
+			System.out.println("----------------------------------------------");
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+			System.out.println("erreur de chargement");
+			main(null);
+		}
+		main(null);
 	}
 
 }
